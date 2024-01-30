27baa25ec322e14717739bb124425ca8a95916b9
Joel Wright <joel.wright@deeperinsights.com>
2024-01-30 09:48:57 +0000

    Add content

---
+++ tests/test_logging.py
@@ -0,0 +1,16 @@
+"""
+Tests for the logging.
+
+Note that it's really difficult to test the logging as pytest nobbles the logger...
+"""
+import logging
+
+from di_logging import configure_logging, get_logger
+
+
+def test_logging():
+    """Do some logging.  We ain't gonna see much though."""
+    configure_logging()
+    logger = get_logger(__name__)
+    logger.info("Test message")
+    logging.info("Python logger")
